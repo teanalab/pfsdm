628dec2916ceb6386011f29b5973afd9d3b2a93c
Fedor Nikolaev <fsqcds@gmail.com>
2015-04-26 12:24:14 -0400

    boxplot output

--- src/main/R/field_experiment.R
+++ src/main/R/field_experiment.R
@@ -1,4 +1,5 @@
 library(reshape2)
 data <- read.table("output/field_experiment.tsv", header = TRUE)
-dataMelt <- melt(data, id=c("qid", "term"),measure.vars=c("names" , "attributes" , "similarentitynames" , "categories" , "outgoingentitynames"))
-boxplot(value~variable,data=dataMelt)
+fields = c("names" , "attributes" , "similarentitynames" , "categories" , "outgoingentitynames")
+dataMelt <- melt(data, id = c("qid", "term"), measure.vars = fields)
+boxplot(value~variable, data = dataMelt)
