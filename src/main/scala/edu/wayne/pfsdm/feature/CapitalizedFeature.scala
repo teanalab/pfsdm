519540ac171ea4f730be251510ec6c510878d776
Fedor Nikolaev <fsqcds@gmail.com>
2015-06-02 22:23:27 -0400

    CapitalizedFeature little refactoring

--- src/main/scala/edu/wayne/pfsdm/feature/CapitalizedFeature.scala
+++ src/main/scala/edu/wayne/pfsdm/feature/CapitalizedFeature.scala
@@ -6,7 +6,11 @@ import edu.wayne.pfsdm.FieldFeature
  * Created by fsqcds on 6/2/15.
  */
 class CapitalizedFeature extends FieldFeature {
+  private def firstLetterUpperCase(token: String): Boolean = {
+    Character.isUpperCase(token.codePointAt(0))
+  }
+
   override def getPhi(tokens: Seq[String], fieldName: String): Double = {
-    if (tokens.forall { token: String => Character.isUpperCase(token.codePointAt(0)) }) 1.0 else 0.0;
+    if (tokens.forall(firstLetterUpperCase)) 1.0 else 0.0
   }
 }
