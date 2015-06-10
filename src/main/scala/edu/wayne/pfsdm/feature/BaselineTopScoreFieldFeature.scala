c6789c46f9c6cc45281343497c8d873d7a5c7a07
Fedor Nikolaev <fsqcds@gmail.com>
2015-06-09 21:07:09 -0400

    Memoize BaselineTopScoreFieldFeature

--- src/main/scala/edu/wayne/pfsdm/feature/BaselineTopScoreFieldFeature.scala
+++ src/main/scala/edu/wayne/pfsdm/feature/BaselineTopScoreFieldFeature.scala
@@ -6,6 +6,7 @@ import org.lemurproject.galago.core.retrieval.query.{Node, StructuredQuery}
 import org.lemurproject.galago.utility.Parameters
 
 import scala.collection.JavaConversions._
+import scala.math.exp
 
 /**
  * Created by fsqcds on 5/1/15.
@@ -19,29 +20,36 @@ class BaselineTopScoreFieldFeature(val traversal: ParametrizedFSDMTraversal) ext
     s"#fieldedsdm(${tokens.mkString(" ")})"
   }
 
+  var memo = Map[(Seq[String], String), Double]()
+
   override def getPhi(tokens: Seq[String], fieldName: String): Double = {
-    val fields = traversal.getFields
-    val fieldWeights: Parameters = Parameters.create
-    val root: Node =
-      if (tokens.size == 1) {
-        fields.foreach { weightedField =>
-          fieldWeights.set(MLMTraversal.UNIGRAM_FIELD_PREFIX + weightedField, if (weightedField == fieldName) 1.0 else 0.0)
-        }
-        StructuredQuery.parse(mlm(Seq(tokens(0))))
-      } else if (tokens.size == 2) {
-        fields.foreach { weightedField =>
-          fieldWeights.set(MLMTraversal.UNIGRAM_FIELD_PREFIX + weightedField, if (weightedField == fieldName) 1.0 else 0.0)
-          fieldWeights.set(FieldedSequentialDependenceTraversal.ORDERED_FIELD_PREFIX + weightedField, if (weightedField == fieldName) 1.0 else 0.0)
-          fieldWeights.set(FieldedSequentialDependenceTraversal.UNWINDOW_FIELD_PREFIX + weightedField, if (weightedField == fieldName) 1.0 else 0.0)
-        }
-        StructuredQuery.parse(fieldedsdm(tokens))
-      } else {
-        throw new IllegalArgumentException("Tokens must be either unigram or bigram")
-      }
-    fieldWeights.copyFrom(traversal.getGlobals)
-    val transformed: Node = traversal.getRetrieval.transformQuery(root, fieldWeights)
-    val results = traversal.getRetrieval.executeQuery(transformed, fieldWeights).scoredDocuments
-    if (results.size > 0) results.head.getScore
-    else Double.NaN
+    memo.get((tokens, fieldName)) match {
+      case Some(phi) => phi
+      case None =>
+        val fields = traversal.getFields
+        val fieldWeights: Parameters = Parameters.create
+        val root: Node =
+          if (tokens.size == 1) {
+            fields.foreach { weightedField =>
+              fieldWeights.set(MLMTraversal.UNIGRAM_FIELD_PREFIX + weightedField, if (weightedField == fieldName) 1.0 else 0.0)
+            }
+            StructuredQuery.parse(mlm(Seq(tokens(0))))
+          } else if (tokens.size == 2) {
+            fields.foreach { weightedField =>
+              fieldWeights.set(MLMTraversal.UNIGRAM_FIELD_PREFIX + weightedField, if (weightedField == fieldName) 1.0 else 0.0)
+              fieldWeights.set(FieldedSequentialDependenceTraversal.ORDERED_FIELD_PREFIX + weightedField, if (weightedField == fieldName) 1.0 else 0.0)
+              fieldWeights.set(FieldedSequentialDependenceTraversal.UNWINDOW_FIELD_PREFIX + weightedField, if (weightedField == fieldName) 1.0 else 0.0)
+            }
+            StructuredQuery.parse(fieldedsdm(tokens))
+          } else {
+            throw new IllegalArgumentException("Tokens must be either unigram or bigram")
+          }
+        fieldWeights.copyFrom(traversal.getGlobals)
+        val transformed: Node = traversal.getRetrieval.transformQuery(root, fieldWeights)
+        val results = traversal.getRetrieval.executeQuery(transformed, fieldWeights).scoredDocuments
+        val phi = if (results.size > 0) exp(results.head.getScore) else 0
+        memo += (tokens, fieldName) -> phi
+        phi
+    }
   }
 }
