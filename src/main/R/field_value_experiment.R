5d45d0156b2b0a9ef5a742602d4db66d30e4831c
Fedor Nikolaev <fsqcds@gmail.com>
2015-06-15 19:10:07 -0400

    Fix merging in field_value_experiment.R

--- src/main/R/field_value_experiment.R
+++ src/main/R/field_value_experiment.R
@@ -2,17 +2,22 @@ library(reshape2)
 library(ggplot2)
 library(dplyr)
 
+fields = c("names" , "attributes" , "similarentitynames" , "categories" , "outgoingentitynames")
+
 tsvPath <- commandArgs(TRUE)[1]
 outputPlot <- commandArgs(TRUE)[2]
+
 data <- read.table(tsvPath, header = TRUE, sep = "\t")
-print(dim(data))
+data$rownumber = 1:nrow(data)
+
 gramtypes <- read.table("src/main/resources/sigir2013-dbpedia/unibigrams-types.tsv", header = TRUE, sep = "\t", quote = "")
-print(dim(gramtypes))
-fields = c("names" , "attributes" , "similarentitynames" , "categories" , "outgoingentitynames")
-dataMerged <- cbind(data, gramtypes, stringAsFactors = TRUE)
+gramtypes$rownumber = 1:nrow(gramtypes)
+
+dataMerged <- merge(data, gramtypes, by = c("rownumber", "qid", "gram"))
 print(dim(dataMerged))
 dataMelt <- melt(dataMerged, id = c("ngramtype", "qid", "gram", "text", "type"), measure.vars = fields)
 dataFilter <- filter(dataMelt, type != "", type != "unsure", type != "none")
+
 p0 = ggplot(dataFilter, aes(y=value, x = variable, fill = type)) + geom_boxplot() + facet_wrap(~ngramtype) +
   theme(axis.text.x=element_text(angle=30,hjust=1)) + scale_y_log10()
 #ylim1 = boxplot.stats(dataBigram$value)$stats[c(1, 5)]
