6d9c75db3e6ff7abd926090988da4ef14329f835
Fedor Nikolaev <fsqcds@gmail.com>
2015-06-17 01:33:20 -0400

    Remove punctuation from queries

--- src/main/scripts/convert_queries_to_json.sh
+++ src/main/scripts/convert_queries_to_json.sh
@@ -12,6 +12,6 @@ input_file=${2:?$usage}
 
 echo '{"queries":['
 
-awk -F $'\t' '{ gsub(/\.|^[[:space:]]*|[[:space:]]*$/, "", $2); print "{\"number\": \""$1"\", \"text\": \"#'$operator'("$2")\"}," }' $input_file
+awk -F $'\t' '{ gsub(/[\.,\?'\''()]|^[[:space:]]*|[[:space:]]*$/, "", $2); gsub(/-/, " ", $2); print "{\"number\": \""$1"\", \"text\": \"#'$operator'("$2")\"}," }' $input_file
 
 echo ']}'
