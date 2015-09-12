1f3441e7eb79026b725da214582c3ebd36240cc5
Fedor Nikolaev <fsqcds@gmail.com>
2015-09-12 02:25:55 -0400

    Using JavaCall for starting optimization

--- scripts/learn-importance.jl
+++ scripts/learn-importance.jl
@@ -1,11 +1,24 @@
+using JavaCall
+
 collections = ["SemSearch_ES", "ListSearch", "INEX_LD", "QALD2"]
 folds = [1:5;]
+wd = pwd()
+refs = RemoteRef[]
 
 for COLLECTION = collections
     for FOLD = folds
-        @spawn (cd("$(homedir())/entity/pfsdm");
-                ENV["JAVA_OPTS"]="-ea -Djava.util.logging.config.file=$(homedir())/entity/pfsdm-logging.properties";
-                run(`sbt "runMain edu.wayne.pfsdm.auxiliary.LearnAndSave ../dbpedia-37-galago-paths.json dbpedia-er/dbpedia-37-galago-config.json dbpedia-er/learn-importance.json dbpedia-er/nikita-queries-wpfsdm/$COLLECTION.cv$FOLD.training.json dbpedia-er/features-scaling.json ../features/weights/learn-uni-fieldlikelihood-nnp.json/$COLLECTION.cv$FOLD.json ../features/weights/learn-bi-fieldlikelihood-baselinetopscore-np-part-nns.json/$COLLECTION.cv$FOLD.json --outputParams=../importance/weights/$COLLECTION.cv$FOLD.json"`))
-        sleep(20)
+        r = @spawn (
+                    cd(wd);
+                    try
+                    JavaCall.init(["-Djava.class.path=$(joinpath(wd, "target", "scala-2.10", "pfsdm-assembly-1.0.jar"))", "-ea", "-Djava.util.logging.config.file=../pfsdm-logging.properties"]);
+                    end;
+                    las = @jimport edu.wayne.pfsdm.auxiliary.LearnAndSave;
+                    jcall(las, "main", Void, (Array{JString, 1},), ["../dbpedia-37-galago-paths.json", "dbpedia-er/dbpedia-37-galago-config.json", "dbpedia-er/learn-importance.json", "dbpedia-er/nikita-queries-wpfsdm/$COLLECTION.cv$FOLD.training.json", "dbpedia-er/features-scaling.json", "../features/weights/learn-uni-fieldlikelihood-nnp.json/$COLLECTION.cv$FOLD.json", "../features/weights/learn-bi-fieldlikelihood-baselinetopscore-np-part-nns.json/$COLLECTION.cv$FOLD.json", "--outputParams=../importance/weights/$COLLECTION.cv$FOLD.json"])
+        )
+        push!(refs, r)
     end
+end
+
+for ref = refs
+    wait(ref)
 end
