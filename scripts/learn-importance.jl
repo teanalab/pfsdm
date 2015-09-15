2baacc568a3f94d12a58f981e43a0afeb6d1ec75
Fedor Nikolaev <fsqcds@gmail.com>
2015-09-15 11:55:29 -0400

    Fix path to properties

--- scripts/learn-importance.jl
+++ scripts/learn-importance.jl
@@ -12,7 +12,7 @@ for feature = features
             r = @spawn (
                         cd(wd);
                         try
-                        JavaCall.init(["-Djava.class.path=$(joinpath(wd, "target", "scala-2.10", "pfsdm-assembly-1.0.jar"))", "-ea", "-Djava.util.logging.config.file=../pfsdm-logging.properties"]);
+                        JavaCall.init(["-Djava.class.path=$(joinpath(wd, "target", "scala-2.10", "pfsdm-assembly-1.0.jar"))", "-ea", "-Djava.util.logging.config.file=./pfsdm-logging.properties"]);
                         end;
                         las = @jimport edu.wayne.pfsdm.auxiliary.LearnAndSave;
                         jcall(las, "main", Void, (Array{JString, 1},), ["../dbpedia-37-galago-paths.json", "dbpedia-er/dbpedia-37-galago-config.json", "dbpedia-er/importance-experiments/learn-importance-collectiontf-$feature.json", "dbpedia-er/nikita-queries-wpfsdm/$COLLECTION.cv$FOLD.training.json", "dbpedia-er/features-scaling.json", "../features/weights/learn-uni-fieldlikelihood-nnp.json/$COLLECTION.cv$FOLD.json", "../features/weights/learn-bi-fieldlikelihood-baselinetopscore-np-part-nns.json/$COLLECTION.cv$FOLD.json", "--outputParams=../importance/weights/$feature/$COLLECTION.cv$FOLD.json"])
