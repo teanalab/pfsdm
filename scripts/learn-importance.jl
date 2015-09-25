d7da1a4b015d8dac7867544154c26454ccc06362
Fedor Nikolaev <fsqcds@gmail.com>
2015-09-25 17:01:38 -0400

    Pass features as arguments

--- scripts/learn-importance.jl
+++ scripts/learn-importance.jl
@@ -1,21 +1,35 @@
 using JavaCall
+using ArgParse
+
+s = ArgParseSettings()
+@add_arg_table s begin
+    "--feature"
+    help = "Features to use"
+    action = :append_arg
+end
+
+parsed_args = parse_args(ARGS, s)
 
 collections = ["SemSearch_ES", "ListSearch", "INEX_LD", "QALD2"]
 folds = [1:5;]
-features = ["categories-binary", "categories-count"]
+features = parsed_args["feature"]
+println(features)
 wd = pwd()
 refs = RemoteRef[]
 
 for feature = features
-    for COLLECTION = collections
-        for FOLD = folds
+    println(feature)
+    mkpath("../importance/weights/$feature")
+    println(feature)
+    for collection = collections
+        for fold = folds
             r = @spawn (
                         cd(wd);
                         try
                         JavaCall.init(["-Djava.class.path=$(joinpath(wd, "target", "scala-2.10", "pfsdm-assembly-1.0.jar"))", "-ea", "-Djava.util.logging.config.file=./pfsdm-logging.properties"]);
                         end;
                         las = @jimport edu.wayne.pfsdm.auxiliary.LearnAndSave;
-                        jcall(las, "main", Void, (Array{JString, 1},), ["../dbpedia-37-galago-paths.json", "dbpedia-er/dbpedia-37-galago-config.json", "dbpedia-er/importance-experiments/learn-importance-collectiontf-$feature.json", "dbpedia-er/nikita-queries-wpfsdm/$COLLECTION.cv$FOLD.training.json", "dbpedia-er/features-scaling.json", "../features/weights/learn-uni-fieldlikelihood-nnp.json/$COLLECTION.cv$FOLD.json", "../features/weights/learn-bi-fieldlikelihood-baselinetopscore-np-part-nns.json/$COLLECTION.cv$FOLD.json", "--outputParams=../importance/weights/$feature/$COLLECTION.cv$FOLD.json"])
+                        jcall(las, "main", Void, (Array{JString, 1},), ["../dbpedia-37-galago-paths.json", "dbpedia-er/dbpedia-37-galago-config.json", "dbpedia-er/importance-experiments/learn-importance-$feature.json", "dbpedia-er/nikita-queries-wpfsdm/$collection.cv$fold.training.json", "dbpedia-er/features-scaling.json", "../features/weights/learn-uni-fieldlikelihood-nnp.json/$collection.cv$fold.json", "../features/weights/learn-bi-fieldlikelihood-baselinetopscore-np-part-nns.json/$collection.cv$fold.json", "--outputParams=../importance/weights/$feature/$collection.cv$fold.json"])
             )
             push!(refs, r)
         end
