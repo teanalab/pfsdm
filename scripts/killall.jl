ad33c6e65647b2df7aaba1849b954c5dc1a4267c
Fedor Nikolaev <fsqcds@gmail.com>
2015-09-30 11:35:48 -0400

    killall takes argument

--- scripts/killall.jl
+++ scripts/killall.jl
@@ -1,5 +1,5 @@
 workers = [1:nworkers();]
 
 for worker = workers
-    @spawnat worker run(`ps aux` |> `grep 'julia'` |> `awk '{print $2}'` |> `xargs kill`)
+    @spawnat worker run(`ps aux` |> `grep $(ARGS[1])` |> `awk '{print $2}'` |> `xargs kill`)
 end
