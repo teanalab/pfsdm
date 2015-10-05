bedec09ef5936df7a7540f147778faffb90dd39c
Fedor Nikolaev <fsqcds@gmail.com>
2015-10-05 16:28:02 -0400

    Two new exeriments

--- scripts/killall.jl
+++ scripts/killall.jl
@@ -1,5 +1,7 @@
 workers = [1:nworkers();]
 
 for worker = workers
-    @spawnat worker run(`ps aux` |> `grep $(ARGS[1])` |> `awk '{print $2}'` |> `xargs kill`)
+    try
+        @spawnat worker run(`ps aux` |> `grep $(ARGS[1])` |> `awk '{print $2}'` |> `xargs kill`)
+    end;
 end
